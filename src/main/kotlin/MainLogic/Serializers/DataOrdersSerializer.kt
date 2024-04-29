0ee45abedea59d649cd586f8cad9d9784cae5755
Alexander Pridava <sanyapridava@mail.ru>
2024-04-30 01:06:03 +0300

    Create empty methods class for serialize/deserialize

---
+++ src/main/kotlin/MainLogic/Serializers/DataOrdersSerializer.kt
@@ -0,0 +1,15 @@
+package org.example.MainLogic.Serializers
+
+class DataOrdersSerializer<T>: Serializer<T> {
+
+    override fun Serialize(dataClassObject: T) {
+
+    }
+
+    override fun Deserialize(jsonString: String) {
+
+
+
+    }
+
+}
