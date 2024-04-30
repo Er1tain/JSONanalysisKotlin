35b73c7ebf193877a94465cff953806d8dd461c1
Alexander Pridava <sanyapridava@mail.ru>
2024-04-30 04:02:59 +0300

    Realize method serialization

--- src/main/kotlin/MainLogic/Serializers/DataOrdersSerializer.kt
+++ src/main/kotlin/MainLogic/Serializers/DataOrdersSerializer.kt
@@ -4,21 +4,34 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.File
 
-class DataOrdersSerializer<T>: Serializer<T> {
+data class Order(
+    val user_id: String,
+    val ordered_at: String,
+    val status: String,
+    val total: String
+)
 
-    override fun Serialize(dataClassObject: T) {
+class DataOrdersSerializer<Order>: Serializer<Order> {
 
+    override fun Serialize(dataClassObjects: List<Order>): String {
+        val serialized = Gson().toJson(dataClassObjects)
+        return serialized
     }
 
-    override fun Deserialize(jsonFile: String): List<T> {
+    override fun Deserialize(jsonFile: String): List<Order> {
+        try {
+            val filePath =
+                "C:\\Users\\Coder\\IdeaProjects\\ProcessingOrder\\src\\main\\kotlin\\data_for_testing\\${jsonFile}"
+            val file = File(filePath)
+            val json = file.readText()
 
-        val filePath = "C:\\Users\\Coder\\IdeaProjects\\ProcessingOrder\\src\\main\\kotlin\\data_for_testing\\${jsonFile}"
-        val file = File(filePath)
-        val json = file.readText()
+            val typeToken = object : TypeToken<List<Order>>() {}.type
+            val units = Gson().fromJson<List<Order>>(json, typeToken)
 
-        val typeToken = object: TypeToken<List<T>>() {}.type
-        val units = Gson().fromJson<List<T>>(json, typeToken)
-        return units
+            return units
+        } catch(e: Exception) {
+            return listOf()
+        }
     }
 
 }
--- src/main/kotlin/MainLogic/Serializers/Serializer.kt
+++ src/main/kotlin/MainLogic/Serializers/Serializer.kt
@@ -2,6 +2,6 @@ package org.example.MainLogic.Serializers
 
 interface Serializer<T> {
 
-    fun Serialize(dataclassobject: T)
+    fun Serialize(dataclassobjects: List<T>): String
     fun Deserialize(jsonFile: String): Any
 }
